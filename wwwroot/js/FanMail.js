70e9335c99984bf77c68a96ce7c6bc67432f1bb3
justiniantheemperor <justinrellis52@gmail.com>
2022-01-05 21:47:27 -0700

    Add project files.

---
+++ wwwroot/js/FanMail.js
@@ -0,0 +1,16 @@
+﻿
+//the default way
+//document.getElementById("btnSend").addEventListener("click", function () {
+//    alert("From: " + document.getElementById("txtFrom").value + 
+//        ", Subject: " + document.getElementById("txtSubject").value +
+//        ", Message: " + document.getElementById("txtMessage").value);
+//    })
+
+//the jquery way
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGreganderson").fadeToggle("slow");
+})
