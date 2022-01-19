d7be13ec20a4a284e8a1f8bc062b11c6e16f531e
justiniantheemperor <justinrellis52@gmail.com>
2022-01-19 10:13:33 -0700

    default app - complete

--- Startup.cs
+++ Startup.cs
@@ -16,13 +16,28 @@ namespace MyFirstASP.netWebApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development")) //can change environment in debug > Debug Properties (bottom option)
+            {
+                app.UseDeveloperExceptionPage(); // can test using the throw command
+            }
+
             app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Blah}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
